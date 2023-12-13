70df7bfb026109704cab38ac1b5e8d86451c6610
Andrea Rivas <andrerivas25@gmail.com>
2023-12-13 16:46:03 -0300

    round, floor, calcular mediana lista impar, ordenar arrays con sort

--- src/platzimath.js
+++ src/platzimath.js
@@ -16,7 +16,28 @@ calcAverage([556, 400, 350, 900, 1100, 5000000]);
 function calcularPromedio(lista) {
     const sumaLista = lista.reduce((total, numero) => total + numero);
     const promedio = sumaLista / lista.length;
-    console.log(promedio);
+    console.log(Math.floor(promedio));
 }
 
 calcularPromedio([556, 400, 350, 900, 1100, 5000000]);
+
+// determinar si una lista es par o impar
+function esPar(list) {
+    return !(list.length % 2);
+}
+
+// Calculando la mediana en una lista impar
+function calcMediana(list) {
+    const listaOrdenada = list.sort((a, b) => a - b);
+    const listaEsPar = esPar(listaOrdenada);
+
+    if (listaEsPar) {
+        return;
+    } else {
+        const indexMitadListaImpar = Math.floor(listaOrdenada.length / 2);
+        const medianaListaImpar = list[indexMitadListaImpar];
+        return medianaListaImpar;
+    }
+}
+
+console.log(calcMediana([50, 20, 30, 50, 10]));
