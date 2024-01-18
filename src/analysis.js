bf624a82883d23b50279c16a8bbff7abc0511da1
Andrea Rivas <andrerivas25@gmail.com>
2024-01-18 13:58:00 -0300

    Proyeccion salarial por persona

--- src/analysis.js
+++ src/analysis.js
@@ -1,4 +1,5 @@
-console.log(salaries);
+// Variables globales
+
 
 // Analisis personal de salarios para alguien en especifico
 
@@ -11,13 +12,41 @@ function findPerson(personInSearch) {
 function medianPerPerson(personName) {
     const jobs = findPerson(personName).jobs;
 
-    const salaries = jobs.map(function (element) {
-        return element.salary;
-    });
+    // Creamos una lista con los salarios
+    const salaries = jobs.map(job => job.salary);
     
+    // Calculamos la mediana
     const medianSalaries = PlatziMath.calcMediana(salaries);
 
     console.log(salaries);
     console.log(medianSalaries);
     return medianSalaries;
+}
+
+function salaryProjection(personName) {
+    const person = findPerson(personName);
+
+    if (!person) {
+        console.log(`Person ${personName} not found`);
+    }
+
+    const jobs = person.jobs;
+
+    // Ordenamos los trabajos por año
+    jobs.sort((a, b) => a.year - b.year);
+
+    let incrementsPorcent = [];
+
+    for (let i = 1; i < jobs.length; i++) {
+        const actualSalary = jobs[i].salary;
+        const lastSalary = jobs[i - 1].salary;
+        const incrementPorcent = (actualSalary - lastSalary) / lastSalary;
+        incrementsPorcent.push(parseFloat(incrementPorcent.toFixed(2)));
+    }
+
+    const medianIncrementPorcent = PlatziMath.calcMediana(incrementsPorcent);
+
+    const currentSalary = jobs.at(-1).salary;
+    const newSalary = currentSalary * medianIncrementPorcent + currentSalary;
+    return Math.round(newSalary);
 }
